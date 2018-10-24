01b268c44dc9c063e915113157cc9bcc6091f875
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-24 12:50:41 -0400

    Tested for strings

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,24 @@
-def pop_on_array(arr)
+def pop_on_empty_array(arr)
   if arr.empty?
-    return "nil"
+    return nil
   end
+else
+  return true
+end
+
+def pop_arr(arr, how_many)
+  returned = []
+  x = 0
+  if pop_on_empty_array(arr)
+    if how_many == 0
+      returned << arr.last
+    else
+      until x == how_many
+        x += 1
+        returned << arr[-1]
+        arr.delete_at(-1)
+      end
+    end
+  end
+  return returned.reverse
 end
--- arrays_tdd.rb
+++ arrays_tdd.rb
@@ -4,6 +4,15 @@ require 'minitest/autorun'
 class Testing_arrays_pop_sort < Minitest::Test
   def test_for_pop_empty_array
     arr = []
-    assert_equal("nil", pop_on_array(arr))
+    assert_equal(nil, pop_on_empty_array(arr))
+  end
+  def test_pop_arr_with_four_elements_return_1
+    arr = [1, 2, 3, 4]
+    assert_equal([4], pop_arr(arr, 0))
+    assert_equal([3, 4], pop_arr(arr, 2))
+  end
+  def test_pop_arr_with_strings
+    arr = ["a", "b", "c"]
+    assert_equal(["c"], pop_arr(arr, 0))
   end
 end
