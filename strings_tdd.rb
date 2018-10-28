586918e016d2f2b65d430395a2c0e255be5a1756
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-28 16:21:03 -0400

    Checking to see if split by commas works and with numbers

--- strings_tdd.rb
+++ strings_tdd.rb
@@ -2,8 +2,12 @@ require_relative 'strings.rb'
 require 'minitest/autorun'
 
 class Testing_strings_split_and_center < Minitest::Test
-  def test_for_split_no_arg
+  def test_for_split_letters_w_spaces
     string = "String splitting action"
-    assert_equal("", my_split(string, " "))
+    assert_equal(["String", "splitting", "action"], my_split(string, " "))
+  end
+  def test_for_split_numbers_w_commas
+    string = "1,5,6,7,8"
+    assert_equal([], my_split(string, ","))
   end
 end
