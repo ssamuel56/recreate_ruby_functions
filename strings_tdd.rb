9a7664e074bda7f6a08a6e2d8e28beb89809e664
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-28 15:05:19 -0400

    First test for string splitting

---
+++ strings_tdd.rb
@@ -0,0 +1,9 @@
+require_relative 'strings.rb'
+require 'minitest/autorun'
+
+class Testing_strings_split_and_center < Minitest::Test
+  def test_for_split_no_arg
+    string = "String splitting action"
+    assert_equal("", my_split(string))
+  end
+end
