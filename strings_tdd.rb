279277788f4252256b15dff9da8328864f95f0e9
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-28 16:32:18 -0400

    Tests pass when splitting by nothing

--- strings_tdd.rb
+++ strings_tdd.rb
@@ -10,4 +10,8 @@ class Testing_strings_split_and_center < Minitest::Test
     string = "1,5,6,7,8"
     assert_equal(["1", "5", "6", "7", "8"], my_split(string, ","))
   end
+  def test_splitting_a_string_with_no_spaces
+    string = "aboy78yy"
+    assert_equal(["a", "b", "o", "y", "7", "8", "y", "y"], my_split(string, ""))
+  end
 end
